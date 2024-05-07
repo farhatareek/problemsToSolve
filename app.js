49c33361386a8d3353caf8ea531189be11d8e45e
farhatareek <118322543+farhatareek@users.noreply.github.com>
2024-05-07 08:03:37 +0300

    some functions added

--- app.js
+++ app.js
@@ -39,3 +39,77 @@ function digitize(n) {
     return n.toString().split("").map((a)=> Number(n)).reverse()
 }
 console.log(digitize(3456789));
+
+
+// counting sheeps 
+
+function countingSheep(array){
+    let initial = 0
+    array.map((m)=> {
+        if(m === true) initial++
+})
+return initial
+}
+console.log(countingSheep([true, false , true , true , true]));
+
+  // get the opposite number
+    function getTheOpposite(num) {
+    return -num
+    }
+    console.log(getTheOpposite(14));
+
+    // change the positive to negative
+    function changeToNegative(number) {
+        return number === -number ? number : -number
+        
+    }
+    console.log(changeToNegative(-14));
+
+    // A Needle in the Haystack
+    function findNeedle(haystack) {
+        return "found the needle at position " +  haystack.indexOf("needle") 
+    }
+    console.log(findNeedle(["junk","n", "needle"]));
+
+    //Count of positives / sum of negatives
+
+    function countPositivesSumNegatives(input) {
+    let positiveArray =[];
+    let negativeSums =0
+    for (let i = 0; i < input.length; i++) {
+        if(input[i]  > 0){
+            positiveArray.push(input[i])
+        }else{
+            negativeSums += input[i]
+        }
+        
+    }
+    return [positiveArray.length, negativeSums]
+    }
+    console.log(countPositivesSumNegatives([1,2,3,4,-6,-5,-4,-3,9,8,7]));
+
+    // Double char
+
+    function doubleChar(str) {
+    return str.split("").map((ell)=> ell.repeat(2)).join("")
+    }
+    console.log(doubleChar("hello"));
+
+    // Basic Mathematical Operations
+
+    function basicOp(operation, value1, value2){
+//  if (operation == '+') {
+//     return value1 + value2
+//  }
+//  if (operation == '-') {
+//     return value1 - value2
+//  }
+//  if (operation == '*') {
+//     return value1 * value2
+//  }
+//  if (operation == '/') {
+//     return value1 / value2
+//  }
+return eval(value1 + operation + value2)
+}
+console.log(basicOp( "*",3, 2));
